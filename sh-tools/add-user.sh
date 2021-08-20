0e91845d7030e7189259dd073d9383176af8c37d
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2021-08-20 13:14:39 +0300

    add user script

---
+++ sh-tools/add-user.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# This script adds user $1 to system with ssh-key $2 if present
+USERNAME="$1"
+
+# Create Home Directory + .ssh Directory
+mkdir -p /home/$USERNAME/.ssh
+
+# Create Authorized Keys File
+touch /home/$USERNAME/.ssh/authorized_keys
+
+# Create User + Set Home Directory
+useradd -d /home/$USERNAME $USERNAME
+
+# Add User to sudo Group
+usermod -aG sudo $USERNAME
+
+# Set Permissions
+chown -R $USERNAME:$USERNAME /home/$USERNAME/
+chown root:root /home/$USERNAME
+chmod 700 /home/$USERNAME/.ssh
+chmod 644 /home/$USERNAME/.ssh/authorized_keys
+
+SSH_KEY=$2
+if [ -v "$SSH_KEY" ]
+then
+  echo "$SSH_KEY" >> /home/$USERNAME/.ssh/authorized_keys
+  echo "No argument supplied"
+fi
